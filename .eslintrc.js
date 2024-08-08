93b34a5e0db40ad9934c8e5a9300f3b9f365058e
Radoslav Popov <neverlastinger@gmail.com>
2024-08-08 19:34:44 +0100

    Update app

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,56 +1,75 @@
-const SUPPORTED_EXTENSIONS = ['.js', '.mjs', '.jsx', '.tsx'];
+const SUPPORTED_EXTENSIONS = [".js", ".mjs", ".jsx", ".tsx"];
 
 module.exports = {
   root: true,
-  extends: 'airbnb',
-  parser: 'babel-eslint',
+  parser: "@babel/eslint-parser",
+  extends: "react-app",
+  parserOptions: {
+    requireConfigFile: false,
+  },
   globals: {
     angular: true,
     PluginManager: true,
-    Visualforce: true
+    Visualforce: true,
   },
   settings: {
-    'import/resolver': {
-      'babel-module': {
-        extensions: SUPPORTED_EXTENSIONS
+    "import/resolver": {
+      "babel-module": {
+        extensions: SUPPORTED_EXTENSIONS,
       },
-      'node': {
-        extensions: SUPPORTED_EXTENSIONS
-      }
+      node: {
+        extensions: SUPPORTED_EXTENSIONS,
+      },
     },
-    'import/extensions': SUPPORTED_EXTENSIONS
+    "import/extensions": SUPPORTED_EXTENSIONS,
   },
   env: {
     browser: true,
     es6: true,
-    jest: true
+    jest: true,
   },
   rules: {
-    'jsx-a11y/label-has-associated-control': ['error', {
-      labelAttributes: ['label'],
-      controlComponents: ['input', 'span'],
-      depth: 3
-    }],
-    'quote-props': 'off',
-    'object-curly-newline': 'off',
-    'jsx-a11y/click-events-have-key-events': 'off',
-    'jsx-a11y/no-static-element-interactions': 'off',
-    'jsx-a11y/no-noninteractive-element-interactions': 'off',
-    'react/forbid-prop-types': 'off',
-    'import/prefer-default-export': 'off',
-    'no-case-declarations': 'off',
-    'arrow-parens': ['error', 'always'],
-    'max-len': ['error', { code: 200, ignorePattern: '^\\s*const\\s.+=\\s*\\(\\{\\s.+\\}\\)\\s.+>', ignoreComments: true }],
-    'no-unused-expressions': ['error', { allowShortCircuit: true, allowTernary: true }],
-    'react/prop-types': 'off',
-    'react/jsx-filename-extension': [1, {
-      extensions: SUPPORTED_EXTENSIONS
-    }],
-    'import/no-extraneous-dependencies': 'off',
-    'no-use-before-define': 'off',
-    'comma-dangle': 'off',
-    'no-nested-ternary': 'off',
-    'react/jsx-one-expression-per-line': 'off',
-    'linebreak-style': 'off'
-  }
-}
+    "jsx-a11y/label-has-associated-control": [
+      "error",
+      {
+        labelAttributes: ["label"],
+        controlComponents: ["input", "span"],
+        depth: 3,
+      },
+    ],
+    "quote-props": "off",
+    "object-curly-newline": "off",
+    "jsx-a11y/click-events-have-key-events": "off",
+    "jsx-a11y/no-static-element-interactions": "off",
+    "jsx-a11y/no-noninteractive-element-interactions": "off",
+    "react/forbid-prop-types": "off",
+    "import/prefer-default-export": "off",
+    "no-case-declarations": "off",
+    "arrow-parens": ["error", "always"],
+    "max-len": [
+      "error",
+      {
+        code: 200,
+        ignorePattern: "^\\s*const\\s.+=\\s*\\(\\{\\s.+\\}\\)\\s.+>",
+        ignoreComments: true,
+      },
+    ],
+    "no-unused-expressions": [
+      "error",
+      { allowShortCircuit: true, allowTernary: true },
+    ],
+    "react/prop-types": "off",
+    "react/jsx-filename-extension": [
+      1,
+      {
+        extensions: SUPPORTED_EXTENSIONS,
+      },
+    ],
+    "import/no-extraneous-dependencies": "off",
+    "no-use-before-define": "off",
+    "comma-dangle": "off",
+    "no-nested-ternary": "off",
+    "react/jsx-one-expression-per-line": "off",
+    "linebreak-style": "off",
+  },
+};
